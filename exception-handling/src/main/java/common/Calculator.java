fcc469e4729ae0ccbb53e4fcd8ea32cf2533a02c
Ivan Petkov <petkow.iv@gmail.com>
2023-09-08 09:02:22 +0300

    Creating Custom Exception topic finished

--- exception-handling/src/main/java/common/Calculator.java
+++ exception-handling/src/main/java/common/Calculator.java
@@ -1,16 +1,27 @@
 package common;
 
+import _3_creating_custom_exceptions.InvalidStatementException;
+
 public class Calculator {
 
-    public static void performOperation(String inputLine) {
-        String[] parts = inputLine.split(" ");
-        MathOperation operation = MathOperation.valueOf(parts[0].toUpperCase());
-        int leftVal = valueFromWord(parts[1]);
-        int rightVal = valueFromWord(parts[2]);
+    public static void performOperation(String inputLine) throws InvalidStatementException {
+        try {
+            String[] parts = inputLine.split(" ");
+            if (parts.length != 3) {
+                throw new InvalidStatementException("Statement must have 3 parts: operation leftVal rightVal");
+            }
+            MathOperation operation = MathOperation.valueOf(parts[0].toUpperCase());
+            int leftVal = valueFromWord(parts[1]);
+            int rightVal = valueFromWord(parts[2]);
 
-        int result = execute(operation, leftVal, rightVal);
+            int result = execute(operation, leftVal, rightVal);
 
-        System.out.println(inputLine + " = " + result);
+            System.out.println(inputLine + " = " + result);
+        } catch(InvalidStatementException ex) {
+            throw ex;
+        } catch(Exception ex) {
+            throw new InvalidStatementException("Error processing statement", ex);
+        }
     }
 
     static int valueFromWord(String word) {
